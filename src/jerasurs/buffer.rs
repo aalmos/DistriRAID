8908763c97e03c08e3beeb06a2d28423c996266a
Adam Homolya <adam.homolya@gmail.com>
2017-07-12 15:33:22 +0200

    made iterator compile

--- src/jerasurs/buffer.rs
+++ src/jerasurs/buffer.rs
@@ -2,19 +2,20 @@ use std::vec::Vec;
 use std::ops::Index;
 use std::slice;
 
-pub struct BlockBuffer {
+pub struct BlockBuffer<'a> {
     _data_blocks: Vec<*mut u8>,
     _parity_blocks: Vec<*mut u8>,
+    _all_blocks_view: Vec<&'a[u8]>,
     _buffer: Vec<u8>,
     _data_size: usize,
     _block_size: usize,
 }
 
-impl BlockBuffer {
+impl<'a> BlockBuffer<'a> {
     pub fn from_data_buffer(mut data_buffer: Vec<u8>,
                             block_size: usize,
                             data_block_count: usize,
-                            parity_block_count: usize) -> BlockBuffer {
+                            parity_block_count: usize) -> BlockBuffer<'a> {
         let original_data_size = data_buffer.len();
         let desired_buffer_size = (data_block_count + parity_block_count) * block_size;
         let padding_size = desired_buffer_size - original_data_size;
@@ -28,10 +29,15 @@ impl BlockBuffer {
             .collect::<Vec<_>>();
 
         let (data_blocks, parity_blocks) = all_blocks.split_at(data_block_count);
+        let all_blocks_view: Vec<_> = data_blocks.iter()
+            .chain(parity_blocks.iter())
+            .map(|p| unsafe { slice::from_raw_parts(*p, block_size) })
+            .collect::<Vec<&[u8]>>();
 
         BlockBuffer {
             _data_blocks: data_blocks.to_vec(),
             _parity_blocks: parity_blocks.to_vec(),
+            _all_blocks_view: all_blocks_view,
             _buffer: data_buffer,
             _data_size: original_data_size,
             _block_size: block_size
@@ -63,24 +69,11 @@ impl BlockBuffer {
     }
 }
 
-impl Index<usize> for BlockBuffer {
-    type Output = [u8];
+impl<'a> Index<usize> for BlockBuffer<'a> {
+    type Output = &'a[u8];
 
-    fn index(&self, index: usize) -> &[u8] {
-        assert!(index < self._data_blocks.len() + self._parity_blocks.len());
-
-        unsafe {
-            if index < self._data_blocks.len() {
-                slice::from_raw_parts(
-                    self._data_blocks[index],
-                    self._block_size
-                )
-            } else {
-                slice::from_raw_parts(
-                    self._parity_blocks[index - self._data_blocks.len()],
-                    self._block_size
-                )
-            }
-        }
+    fn index(&self, index: usize) -> &Self::Output {
+        assert!(index < self._all_blocks_view.len());
+        &self._all_blocks_view[index]
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,33 +20,4 @@ fn main() {
     for i in 0..result.total_block_count() as usize {
         println!("{:?}", result[i]);
     }
-/*
-    //let input = [42u8; 15000];
-    let (data, parity) = codec.encode(&rstr.as_bytes());
-
-    let data_ptrs = data.as_ptr();
-
-    for i in 0..data.block_count() as isize {
-        unsafe {
-            let block = *data_ptrs.offset(i);
-            let buffer: Vec<_> = (0..data.block_size() as isize).map(|j| *block.offset(j)).collect();
-            println!("{:?}", buffer);
-        }
-    }
-
-    println!("parity");
-
-    for i in 0..parity.block_count() as isize {
-        unsafe {
-            let block = *data_ptrs.offset(i);
-            let buffer: Vec<_> = (0..parity.block_size() as isize).map(|j| *block.offset(j)).collect();
-            println!("{:?}", buffer);
-        }
-    }
-
-    //let mut output = [1u8, 10];
-    //let input = [0u8, 10];
-
-    //codec.encode(&input, &mut output);
-*/
 }
